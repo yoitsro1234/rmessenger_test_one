7dc1105032858fd4d5bf3659285364e3f3f5c627
yoitsro1234 <72605391+yoitsro1234@users.noreply.github.com>
2020-12-01 20:06:51 +0530

    Add files via upload

---
+++ messenger_page.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCwwkpgzvZcF-6P91q9tYuLqfZQLjloOJE",
+    authDomain: "rmessenger-385ca.firebaseapp.com",
+    databaseURL: "https://rmessenger-385ca.firebaseio.com",
+    projectId: "rmessenger-385ca",
+    storageBucket: "rmessenger-385ca.appspot.com",
+    messagingSenderId: "1046848051167",
+    appId: "1:1046848051167:web:10784099362bafeb04d66a"
+  };
+  firebase.initializeApp(firebaseConfig);
